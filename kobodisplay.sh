ce06e845eaf13a6fa174073d79b35e5e21892119
tux-linux <63172576+tux-linux@users.noreply.github.com>
2020-08-25 14:37:43 -0400

    Update kobodisplay.sh

--- kobodisplay.sh
+++ kobodisplay.sh
@@ -3,4 +3,5 @@ while :; do read -r -p 'IP: ' ip && break; done
 while :; do read -r -p "Your Kobo device screen width resolution: " width && break; done
 while :; do read -r -p "Your Kobo device screen height resolution: " height && break; done
     sshpass -p test ssh root@$ip 'watch -n 30 "fbink -H -k -f | tee -a output.txt" &>/dev/null &'
-    while true; do gnome-screenshot -p --file=/tmp/continuous.png && sshpass -p test scp /tmp/continuous.png root@$ip:/tmp && sshpass -p test ssh root@$ip 'fbink -g file=/tmp/continuous.png,w='$width',h='$height''; done
+    sshpass -p test ssh root@$ip 'watch -n 0.5 "fbink -g file=/tmp/continuous.png,w='$width',h='$height'| tee -a output.txt" &>/dev/null &'
+    while true; do gnome-screenshot -p --file=/tmp/continuous.png && sshpass -p test scp /tmp/continuous.png root@$ip:/tmp ; done
